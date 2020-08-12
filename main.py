da2298c9217c25018e88edfc80d0dbb31151f40e
Pauline Barmby <pbarmby@uwo.ca>
2020-08-12 11:10:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+for i in range(0,10):
+  print(i)
+print("all done")
+import numpy as np
+a = np.ones((5,5))
+print(a[1:3,1:3])
+
+def myfunc(x=27, prime = True):
+  return(x/3)
+
+print(myfunc(6))
