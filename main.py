44f29733f290e5a0bd2867cd8131a04fe0a02505
Pauline Barmby <pbarmby@uwo.ca>
2020-08-12 15:22:23 +0000

    refactor myfunc

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-for i in range(0,10):
-  print(i)
-print("all done")
-import numpy as np
-a = np.ones((5,5))
-print(a[1:3,1:3])
+#for i in range(0,10):
+#  print(i)
+#print("all done")
+#import numpy as np
+#a = np.ones((5,5))
+#print(a[1:3,1:3])
 
-def myfunc(x=27, prime = True):
-  return(x/3)
+def myfunc(x=27, checkprime = True):
+  if checkprime:
+    isprime = True
+    for i in range(2,x):
+      if x % i == 0:
+        isprime = False
+        break
+    return(isprime)
+  else:
+    return(x/3)
 
-print(myfunc(6))
+print(myfunc(29, checkprime=False))
